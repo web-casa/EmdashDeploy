43e2b4b7485610ddbb44848125cd10be7c642d5f
yeagoo <i90h@vip.qq.com>
2026-04-08 13:52:41 +0800

    Read installer prompts from tty when available

--- lib/prompt.sh
+++ lib/prompt.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 
+prompt_input_source() {
+	if [[ -t 0 ]]; then
+		printf '/dev/stdin\n'
+		return
+	fi
+
+	if [[ -r /dev/tty ]]; then
+		printf '/dev/tty\n'
+		return
+	fi
+
+	printf '\n'
+}
+
 prompt_value() {
 	local var_name="$1"
 	local label="$2"
 	local default_value="$3"
 	local input=""
+	local prompt_source=""
 	local -n ref="${var_name}"
 
-	if [[ ! -t 0 ]]; then
+	prompt_source="$(prompt_input_source)"
+	if [[ -z "${prompt_source}" ]]; then
 		ref="${default_value}"
 		return
 	fi
 
-	read -r -p "${label} [${default_value}]: " input
+	read -r -p "${label} [${default_value}]: " input <"${prompt_source}"
 	ref="${input:-${default_value}}"
 }
 
@@ -22,6 +38,7 @@ prompt_secret() {
 	local default_value="$3"
 	local input=""
 	local masked_default=""
+	local prompt_source=""
 	local -n ref="${var_name}"
 
 	if [[ -n "${default_value}" ]]; then
@@ -30,12 +47,13 @@ prompt_secret() {
 		masked_default="$(ti blank_word)"
 	fi
 
-	if [[ ! -t 0 ]]; then
+	prompt_source="$(prompt_input_source)"
+	if [[ -z "${prompt_source}" ]]; then
 		ref="${default_value}"
 		return
 	fi
 
-	read -r -s -p "${label} ${masked_default}: " input
+	read -r -s -p "${label} ${masked_default}: " input <"${prompt_source}"
 	printf '\n'
 	ref="${input:-${default_value}}"
 }
@@ -46,19 +64,21 @@ prompt_yes_no() {
 	local default_value="$3"
 	local input=""
 	local pretty_default="y"
+	local prompt_source=""
 	local -n ref="${var_name}"
 
 	if [[ "${default_value}" != "1" ]]; then
 		pretty_default="n"
 	fi
 
-	if [[ ! -t 0 ]]; then
+	prompt_source="$(prompt_input_source)"
+	if [[ -z "${prompt_source}" ]]; then
 		ref="${default_value}"
 		return
 	fi
 
 	while true; do
-		read -r -p "${label} [y/n, $(ti default_word) ${pretty_default}]: " input
+		read -r -p "${label} [y/n, $(ti default_word) ${pretty_default}]: " input <"${prompt_source}"
 		input="${input:-${pretty_default}}"
 		case "${input,,}" in
 		y | yes)
@@ -81,9 +101,11 @@ prompt_choice() {
 	local default_value="$4"
 	local input=""
 	local -a option_list=()
+	local prompt_source=""
 	local -n ref="${var_name}"
 
-	if [[ ! -t 0 ]]; then
+	prompt_source="$(prompt_input_source)"
+	if [[ -z "${prompt_source}" ]]; then
 		ref="${default_value}"
 		return
 	fi
@@ -91,7 +113,7 @@ prompt_choice() {
 	IFS=' ' read -r -a option_list <<<"${options}"
 
 	while true; do
-		read -r -p "${label} [${options}] ($(ti default_word) ${default_value}): " input
+		read -r -p "${label} [${options}] ($(ti default_word) ${default_value}): " input <"${prompt_source}"
 		input="${input:-${default_value}}"
 		for option in "${option_list[@]}"; do
 			if [[ "${input}" == "${option}" ]]; then
@@ -105,13 +127,15 @@ prompt_choice() {
 
 prompt_confirm_dns_ready() {
 	local input=""
+	local prompt_source=""
 
-	if [[ ! -t 0 ]]; then
+	prompt_source="$(prompt_input_source)"
+	if [[ -z "${prompt_source}" ]]; then
 		return
 	fi
 
 	while true; do
-		read -r -p "$(ti https_dns_confirm) [y/n]: " input
+		read -r -p "$(ti https_dns_confirm) [y/n]: " input <"${prompt_source}"
 		case "${input,,}" in
 		y | yes)
 			return
